96f7eae0b40949f92b76a81e977afc49eb3d4647
AnilChinchawale <Anil24593@gmail.com>
2019-02-09 11:17:01 +0530

    Installing IPFS, GoLang added separately.

---
+++ install_IPFS.sh
@@ -0,0 +1,12 @@
+Installing IPFS, GoLang
+
+sudo apt-get update
+sudo apt-get install golang-go -y
+wget https://dist.ipfs.io/go-ipfs/v0.4.13/go-ipfs_v0.4.13_linux-amd64.tar.gz
+tar xvfz go-ipfs_v0.4.13_linux-amd64.tar.gz
+sudo mv go-ipfs/ipfs /usr/local/bin/ipfs
+
+
+
+rm -R go-ipfs_v0.4.13_linux-amd64.tar.gz
+sudo mv ./go-ipfs $home
